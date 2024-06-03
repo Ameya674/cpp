4434be08fbf20aba5703fb35699f8c100f6231fd
Ameya <ameyaprofessional247@gmail.com>
2024-06-03 18:30:33 +0530

    arithmetic operators

---
+++ arith_operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    int students = 20;
+    students += 1;
+    students -= 1;
+    students /= 2;
+    students *= 2;
+
+    // moudulo - remainder
+    int remainder = students % 3;
+
+    return 0;
+}
