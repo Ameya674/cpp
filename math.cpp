83c2ad22e4bb4b5f50d9b89ebcf93cfae118f224
Ameya <ameyaprofessional247@gmail.com>
2024-06-20 13:48:48 +0530

    basic math functions in C++

---
+++ math.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main () {
+
+    double x = 3;
+    double y = 4;
+
+    double z = pow(2,4);
+    double w = sqrt(9);
+    double max_ = max(x, y);
+    double min_ = min(x, y);
+    double abs_ = abs(-3);
+
+    return 0;
+}
